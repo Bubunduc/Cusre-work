7407956a0a05a2b99900bd95e975673e986b3557
Bubunduc <86464565+Bubunduc@users.noreply.github.com>
2022-12-15 01:44:14 +0300

    Update ConsoleApplication1.c

--- ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -4,17 +4,19 @@
 #include <string.h>
 #include <io.h>
 #pragma warning(disable:4996)
+#define SIZE   10;
 struct sportsmen {
 	char full_name[60];
 	float results[6];
 	float maximum;
 };
-void results(int size, struct sportsmen sport[]);
-void winners(int size, struct sportsmen sport[]);
-float kicked(int size, struct sportsmen sport[]);
-float average(int size, struct sportsmen sport[]);
-float local_maximum(struct  sportsmen sportsmen);
-void sort(int size, struct sportsmen sport[]);
+float local_maximum(float scores[], int diap);
+void sort(int size, char names[10][40], float scores[10][7]);
+void results(int size, char names[10][40], float scores[10][7]);
+void winners(int size, char names[10][40], float scores[10][7]);
+float kicked(int size, char names[10][40], float scores[10][7]);
+float average(int size, float scores[10][7]);
+
 
 
 int main()
@@ -33,8 +35,7 @@ int main()
 	gets(file_name);//Чтобы имена спортсменов корректно считались необходимо сменить кодировку с UTF-8 на ANCII
 	strcat(file_name, extension);
 	printf("Имя файла: %s\n", file_name);
-	int what_do = 0; int size = 0;
-
+	int what_do = 0; 
 	FILE* table;
 	table = fopen(file_name, "rt");
 	if (table == NULL) {
@@ -42,32 +43,27 @@ int main()
 		fclose(table);
 		exit(0);
 	}
-	while (!feof(table)) {
-		if (fgetc(table) == '\n')
-			size += 1;
-	}
-	fclose(table);
-	table = fopen(file_name, "rt");
-
-	sportsmens = (struct sportsmen*)malloc(size * sizeof(struct sportsmen));
-
-
-	for (int i = 0; i < size + 1; i++) {
-		fscanf(table, "%s", &sportsmens[i].full_name);
-		fscanf(table, " %5f ", &sportsmens[i].results[0]);
-		fscanf(table, " %5f ", &sportsmens[i].results[1]);
-		fscanf(table, " %5f ", &sportsmens[i].results[2]);
-		fscanf(table, " %5f ", &sportsmens[i].results[3]);
-		fscanf(table, " %5f ", &sportsmens[i].results[4]);
-		fscanf(table, " %5f\n", &sportsmens[i].results[5]);
-	}
+	float scores[10][7];
+	char names[10][40];
 	
+	
+	for (int i = 0; i < 10; i++) {
+		
+		fscanf(table, "%s ", &names[i]);
+		fscanf(table, " %5f ", &scores[i][0]);
+		fscanf(table, " %5f ", &scores[i][1]);
+		fscanf(table, " %5f ", &scores[i][2]);
+		fscanf(table, " %5f ", &scores[i][3]);
+		fscanf(table, " %5f ", &scores[i][4]);
+		fscanf(table, " %5f\n", &scores[i][5]);
+
+	}
 	
 	int is_sorted = 0;
-
+	int size = SIZE;
 	while (what_do != 5) {
 		if (is_sorted == 0) {
-			sort(size, sportsmens);
+			sort(size,names,scores);
 			is_sorted += 1;
 		}
 		printf("\nВот что может программа:\n1)Вывести таблицу результатов;\n2)Вывести список победителей;\n3)Вывести выбывших игроков\n4)Показать средний арифметический результат\n5)Выйти из программы\n6)Очистить консоль\n");
@@ -78,17 +74,17 @@ int main()
 		case 1: {
 			puts("Результаты спортсменов,Каждая строка - это результаты спортсмена за 6 попыток");
 			
-			results(size, sportsmens);
+			results(size,names, scores);
 			break;
 		}
-		case 2:  winners(size, sportsmens); break;
-		case 3:  kicked(size, sportsmens); break;
+		case 2:  winners(size, names, scores); break;
+		case 3:  kicked(size, names, scores); break;
 		case 4: {
-			float averag = average(size, sportsmens);
-			printf("Среднее арифметическое из всех рузультатов спортсменов %5.2f\n", averag);
+		
+			printf("Среднее арифметическое из всех рузультатов спортсменов %5.2f\n", average(size, scores));
 			break;
 		}
-		case 5: puts("Выход совершен успешно!"); free(sportsmens); break;
+		case 5: puts("Выход совершен успешно!"); return 0;
 		case 6: system("cls"); puts("Очистка прошла успешно!"); break;
 		default:
 			printf("Неизвестная программа");
@@ -98,56 +94,72 @@ int main()
 }
 
 
-float local_maximum(struct  sportsmen sportsmen)
+float local_maximum(float scores[], int diap)
 {
 	float max = 0.0;
-	for (int i = 0; i < 4; i++) {
-		if (sportsmen.results[i] > max) {
-			max = sportsmen.results[i];
+	int size = SIZE;
+	for (int i = 0; i < diap; i++) {
+		if (scores[i] > max) {
+			max = scores[i];
 		}
 	}
 	return max;
 }
-void sort(int size, struct sportsmen sport[]) {
+void sort(int size, char names[10][40], float scores[10][7]) {
 	setlocale(LC_ALL, "rus");
-	for (int i = 0; i < size + 1; i++) {
-		sport[i].maximum = local_maximum(sport[i]);
-	}
+	
 	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size - i; j++) {
-			if (sport[j + 1].maximum > sport[j].maximum) {
-				struct sportsmen buffer = sport[j + 1];
-				sport[j + 1] = sport[j];
-				sport[j] = buffer;
+		scores[i][6] = local_maximum(scores[i], 3);
+	}
+	int max_index = 0;
+	for (int i = 0; i < size; i++) {
+		max_index = i;
+			for (int j = i+1; j < size;j++) {
+				if (scores[max_index][6] < scores[j][6]) {
+					max_index = j;
+				}
 			}
+	float buffer[7];
+	for (int buff = 0; buff < 7; buff++) {
+			buffer[buff] = scores[max_index][buff];
+			scores[max_index][buff] = scores[i][buff];
+			scores[i][buff] = buffer[buff];
+			}
+		char name_buff[40];
+		strcpy(name_buff, names[max_index]);
+		strcpy(names[max_index], names[i]);
+		strcpy(names[i], name_buff);
+
+	}
+	for (int i = 8; i < size; i++) {
+		for (int j = 3; j < 7; j++) {
+			scores[i][j] = 0.0;
 		}
 	}
 
 }
-void results(int size, struct sportsmen sport[]) {
+void results(int size, char names[10][40], float scores[10][7]) {
 	setlocale(LC_ALL, "rus");
 
-	for (int i = 1; i < size + 2; i++) {
-		printf("\n\n");
-		for (int j = 0; j < 7; j++) {
+	for (int i = 0; i < size; i++) {
+		printf("\n");
+		for (int j = 0; j < 6; j++) {
 			if (j == 0) {
-				printf("%1d %5s ", i, sport[i - 1].full_name);
+				printf("%2d %1s ",i+1, names[i]);
 			}
-			else
-			{
-				printf("%5.2f ", sport[i - 1].results[j - 1]);
-			}
+			printf(" %5.2f ", scores[i][j]);
 		}
 	}
+	
 	printf("\n\n");
 }
-void winners(int size, struct sportsmen sport[]) {
+void winners(int size, char names[10][40], float scores[11][7]) {
 	float* maximums;
 	maximums = (float*)malloc(size * sizeof(float));
 	float winners_score[3];
 	int winner_index[3];
 	for (int i = 0; i < 8; i++) {
-		maximums[i] = local_maximum(sport[i]);
+		maximums[i] = local_maximum(scores[i], 7);
 	}
 	float max_1 = 0.0;
 	float max_2 = 0.0;
@@ -176,38 +188,37 @@ void winners(int size, struct sportsmen sport[]) {
 	winners_score[2] = max_3;
 	printf("Победители:\n");
 	for (int i = 1; i < 4; i++) {
-		printf("%d место: %s с результатом %5.2f\n", i, sport[winner_index[i - 1]].full_name, winners_score[i - 1]);
+		printf("%d место: %s с результатом %5.2f\n", i, names[winner_index[i - 1]], winners_score[i - 1]);
 	}
 	free(maximums);
 }
 
-float kicked(int size, struct sportsmen sport[]) {
+float kicked(int size,char names[10][40], float scores[10][7]) {
 	puts("Проигравшие(Набравшие наихудшие результаты за первые 3 попытки)");
-	for (int i = 8; i < size + 1; i++) {
+	for (int i = 8; i < size; i++) {
 		printf("\n\n");
-		for (int j = 0; j < 7; j++) {
+		for (int j = 0; j < 6; j++) {
 			if (j == 0) {
-				printf("%1d %5s ", i+1, sport[i].full_name);
+				printf("%d %s ", i + 1, names[i]);
 			}
-			else
-			{
-				printf("%5.2f ", sport[i].results[j]);
-			}
+			printf(" %5.2f",scores[i][j]);
 		}
 		
 	}
 	printf("\n");
 	
 }
-float average(int size, struct sportsmen sport[]) {
+float average(int size, float scores[10][7]) {
 	float summ = 0.0;
 	float average = 0.0;
-	for (int i = 0; i < size + 1; i++) {
+	
+	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < 6; j++) {
-			summ += sport[i].results[j];
+			summ += scores[i][j];
 		}
 	}
 	average = summ / ((float)size * 6.0);
 	return average;
 }
+
 
